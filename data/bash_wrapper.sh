5ab3e38c6e0e2d049e55f0c6cf246fc0b32c8f2a
PRAGMA <17136956+imPRAGMA@users.noreply.github.com>
2019-12-11 03:50:32 +0000

    Update bash_wrapper.sh

--- data/bash_wrapper.sh
+++ data/bash_wrapper.sh
@@ -6,6 +6,6 @@ export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 DEVICE=$1
 USERNAME=pragma
-OUTPUT_D=~/Videos  # Change this! Just remember ~ will point to the home directory of the USERNAME you set.
+OUTPUT_D=/home/owner/Videos  # Change this! Beware of using `~` as it often can fail unexpectedly
 
 /bin/su -l -c "echo /usr/bin/python3 /opt/udfbackup/__init__.py -d $DEVICE -f $OUTPUT_D | at now" -s /bin/bash $USERNAME
