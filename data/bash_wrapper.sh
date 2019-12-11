ac5dcca1b774f80521184944e43484e7c92d728c
PRAGMA <17136956+imPRAGMA@users.noreply.github.com>
2019-12-11 03:44:45 +0000

    Update bash_wrapper.sh

--- data/bash_wrapper.sh
+++ data/bash_wrapper.sh
@@ -4,7 +4,8 @@ export XAUTHORITY=/home/owner/.Xauthority
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
+DEVICE=$1
 USERNAME=pragma
 OUTPUT_D=~/Videos  # Change this! Just remember ~ will point to the home directory of the USERNAME you set.
 
-/bin/su -l -c "echo /usr/bin/python3 /opt/udfbackup/__init__.py -d $1 | at now" -s /bin/bash $USERNAME
+/bin/su -l -c "echo /usr/bin/python3 /opt/udfbackup/__init__.py -d $DEVICE -f $OUTPUT_D | at now" -s /bin/bash $USERNAME
